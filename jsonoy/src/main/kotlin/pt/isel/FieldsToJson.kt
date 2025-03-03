5356067f9283800917ed2a87f16fc66481e2a01c
Diogo Arnauth Fernandes Leitão <dioarnauth@gmail.com>
2025-03-03 18:26:52 +0000

    ultima

--- jsonoy/src/main/kotlin/pt/isel/FieldsToJson.kt
+++ jsonoy/src/main/kotlin/pt/isel/FieldsToJson.kt
@@ -4,20 +4,20 @@ import kotlin.reflect.KProperty
 import kotlin.reflect.full.memberProperties
 
 fun Any.fieldsToJson(): String {
-    val jsonPairs = this::class.memberProperties.map { field ->
-        propToJson(field, this)
+    val jsonPairs = this::class.memberProperties.map { properties ->
+        propToJson(properties, this)
     }.filter { it.isNotEmpty() }
 
     return "{${jsonPairs.joinToString(",")}}"
 }
 
-private fun propToJson(field: KProperty<*>, obj: Any): String {
-    val propValue = field.getter.call(obj) ?: return """"${field.name}":null"""
+private fun propToJson(properties: KProperty<*>, obj: Any): String {
+    val propValue = properties.getter.call(obj) ?: return """"${properties.name}":null"""
 
     return when (propValue) {
-        is String -> """"${field.name}":"$propValue""""
-        is Number, is Boolean -> """"${field.name}":$propValue"""
-        is List<*> -> """"${field.name}":[${propValue.joinToString(",") { it?.fieldsToJson() ?: "null" }}]"""
-        else -> """"${field.name}":${propValue.fieldsToJson()}"""
+        is String -> """"${properties.name}":"$propValue""""
+        is Number, is Boolean -> """"${properties.name}":$propValue"""
+        is List<*> -> """"${properties.name}":[${propValue.joinToString(",") { it?.fieldsToJson() ?: "null" }}]"""
+        else -> """"${properties.name}":${propValue.fieldsToJson()}"""
     }
 }
