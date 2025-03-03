29eb50089b2d8de40475cb0b2a2fe61b045d6c9f
Diogo Arnauth Fernandes Leitão <dioarnauth@gmail.com>
2025-03-03 16:26:12 +0000

    Update FieldsToJson.kt

--- jsonoy/src/main/kotlin/pt/isel/FieldsToJson.kt
+++ jsonoy/src/main/kotlin/pt/isel/FieldsToJson.kt
@@ -1,5 +1,23 @@
 package pt.isel
 
+import kotlin.reflect.KProperty
+import kotlin.reflect.full.memberProperties
+
 fun Any.fieldsToJson(): String {
-    TODO()
+    val jsonPairs = this::class.memberProperties.map { prop ->
+        propToJson(prop, this)
+    }.filter { it.isNotEmpty() }
+
+    return "{${jsonPairs.joinToString(",")}}"
+}
+
+private fun propToJson(prop: KProperty<*>, target: Any): String {
+    val propValue = prop.getter.call(target) ?: return """"${prop.name}":null"""
+
+    return when (propValue) {
+        is String -> """"${prop.name}":"$propValue""""
+        is Number, is Boolean -> """"${prop.name}":$propValue"""
+        is List<*> -> """"${prop.name}":[${propValue.joinToString(",") { it?.fieldsToJson() ?: "null" }}]"""
+        else -> """"${prop.name}":${propValue.fieldsToJson()}"""
+    }
 }
