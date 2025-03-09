2987cfb3fe015608b8a4e1c3efa80c59c5c31a68
Diogo Arnauth Fernandes Leitão <dioarnauth@gmail.com>
2025-03-09 23:33:05 +0000

    ultima

--- jsonoy/src/main/kotlin/pt/isel/MembersToJson.kt
+++ jsonoy/src/main/kotlin/pt/isel/MembersToJson.kt
@@ -1,5 +1,38 @@
 package pt.isel
 
+import kotlin.reflect.KProperty
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.KProperty1
+import kotlin.reflect.full.findAnnotation
+
+
 fun Any.membersToJson(): String {
-    TODO()
+    val jsonPairs = this::class.memberProperties.map { property ->
+        propToJson(property, this)
+    }.filter { it.isNotEmpty() }
+
+    val result =  "{${jsonPairs.joinToString(",")}}"
+    println("ahhhhhhhhhhhhhhhhhhhhhhhhhhh $result" )
+    return result
+}
+
+private fun propToJson(property: KProperty1<*, *>, obj: Any): String {
+    val propValue = property.getter.call(obj) ?: return """"${property.name}":null"""
+
+
+    val jsonKey = property.findAnnotation<ToJsonPropName>()?.name ?: property.name
+    println("jsonKey $jsonKey")
+
+    val formatterClass = property.findAnnotation<ToJsonFormatter>()?.formatter
+    println("formatterClass $formatterClass")
+
+    val formattedValue = formatterClass?.objectInstance?.format(propValue) ?: propValue
+    println("formattedValue $formattedValue")
+
+    return when (formattedValue) {
+        is String -> """"$jsonKey":"$formattedValue""""
+        is Number, is Boolean -> """"$jsonKey":$formattedValue"""
+        is List<*> -> """"$jsonKey":[${formattedValue.joinToString(",") { it?.membersToJson() ?: "null" }}]"""
+        else -> """"$jsonKey":${formattedValue.membersToJson()}"""
+    }
 }
